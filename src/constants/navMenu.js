5ce55a10b77c5bf2ecf5b4f6eeb44f00e03abdce
britant <britantshrestha@gmail.com>
2025-06-22 16:36:28 +0545

    cleanup

--- src/constants/navMenu.js
+++ src/constants/navMenu.js
@@ -1,21 +1,9 @@
-import { ABOUT_ROUTE, CONTACT_ROUTE, HOME_ROUTE, LOGIN_ROUTE, PRODUCTS_ROUTE, REGISTER_ROUTE } from "./route";
+import { HOME_ROUTE, LOGIN_ROUTE, REGISTER_ROUTE } from "./route";
 
 const navMenu = [
     {
         route: HOME_ROUTE,
         label: 'Home',
-    },
-    {
-        route: ABOUT_ROUTE,
-        label: 'About',
-    },
-    {
-        route: CONTACT_ROUTE,
-        label: 'Contact',
-    },
-    {
-        route: PRODUCTS_ROUTE,
-        label: 'Products',
     },
     {
         route: LOGIN_ROUTE,
