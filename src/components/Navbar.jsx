c96a9bb2b9e5bc821432478c6f6fbc3deb54591b
britant <britantshrestha@gmail.com>
2025-06-22 15:02:07 +0545

    title updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,12 +12,12 @@ const Navbar = () => {
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
           <img
-            src="https://flowbite.com/docs/images/logo.svg"
+            src="logo.svg"
             className="h-8"
-            alt="Flowbite Logo"
+            alt="Keydash Logo"
           />
           <span className="self-center text-2xl font-semibold whitespace-nowrap text-black">
-            Flowbite
+            Keydash
           </span>
         </a>
         <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
